40fbc5f2aac5d85a90bba3e330f3d6a435a22bf5
Carlos Furigo <cfurigo@coachit.com.br>
2024-09-10 15:53:59 +0000

    First commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.sales.report.salesorderreport.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
